998a3ebf69c58ac97685abbe4069bf04075863c4
Edward John <ejgauci51@gmail.com>
2021-01-30 22:24:45 +0100

    obstacles move, level 3 done

--- Scripts/patrolObstacles.cs
+++ Scripts/patrolObstacles.cs
@@ -11,6 +11,8 @@ public class patrolObstacles : MonoBehaviour
 
     public List<Transform> waypoints;
 
+    bool reverse = false;
+
 
     void Start()
     {
@@ -19,16 +21,28 @@ public class patrolObstacles : MonoBehaviour
         StartCoroutine(Move());
     }
 
-   IEnumerator Move()
+    IEnumerator Move()
     {
         while (true)
         {
-            if (this.transform.position != waypoints[currentWayPoint].position)
+            if (this.transform.position != waypoints[currentWayPoint].position )
             {
-                print("obstacle moving");
+                //print("obstacle moving");
                 this.transform.position = Vector3.MoveTowards(this.transform.position, waypoints[currentWayPoint].position, 1f);
                 gm.Scan();
-                yield return new WaitForSeconds(0.5f);
+                yield return new WaitForSeconds(1);
+
+            }
+            else if(reverse==true)
+            {
+                if(this.transform.position == waypoints[0].position)
+                {
+                    reverse = false;
+                }
+                else{
+                    currentWayPoint--;
+                }
+                
 
             }
             else
@@ -38,12 +52,15 @@ public class patrolObstacles : MonoBehaviour
 
             if (currentWayPoint >= waypoints.Count)
             {
-                currentWayPoint = 0;
-                yield return new WaitForSeconds(1);
+                currentWayPoint --;
+                reverse = true;
+                //yield return new WaitForSeconds(1);
+
 
             }
         }
 
-        
-    }
+     }
+
+    
 }
