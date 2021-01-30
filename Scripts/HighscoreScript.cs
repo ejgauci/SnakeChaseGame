67aa83e5a263c70690c557da4eea3e05f7d5b1cc
Edward John <ejgauci51@gmail.com>
2021-01-30 14:50:30 +0100

    edited level 2, added highscore script

---
+++ Scripts/HighscoreScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class playerHighScores
+{
+    public string n;
+    public float t;
+}
+public class HighscoreScript : MonoBehaviour
+{
+
+    public string[] names;
+    public float[] times;
+
+    GameManager gm;
+    playerHighScores pHS;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {/*
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+        names = PlayerPrefsX.GetStringArray("Names", "", 1);
+        times = PlayerPrefsX.GetFloatArray("Times", 0f, 1);
+
+        if(names[0] == "")
+        {
+            names[0] = gm.username;
+            times[0] = gm.time;
+
+            PlayerPrefsX.SetStringArray("Names", names);
+            PlayerPrefsX.SetFloatArray("Times", times);
+        }
+        else{
+            pHS = new playerHighScores();
+            List<playerHighScores> playerHSList = new List<playerHighScores>();
+
+            for(int i = 0; i < names.Length; i++){
+                pHS = new playerHighScores();
+                pHS.n = names[i];
+                pHS.t = times[i];
+                playerHSList.Add(pHS);
+            }
+        }
+    */}
+
+}
